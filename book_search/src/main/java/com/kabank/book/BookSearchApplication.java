e6a2ba16335f50b7b63374dd45b1440a87192bf9
hhlyn0328 <41069113+hhlyn0328@users.noreply.github.com>
2018-07-13 03:03:24 +0900

    1. User, History Model 생성 2. 기본 Repository 생성 및 Test 3. 책 API 서비스 구현 및 Test 4. API 연결정보 관련 properties 파일 분리 5. cotroller & view mapping 테스트 용 기본 controller, view 생성

--- book_search/src/main/java/com/kabank/book/BookSearchApplication.java
+++ book_search/src/main/java/com/kabank/book/BookSearchApplication.java
@@ -2,8 +2,10 @@ package com.kabank.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:api.properties")
 public class BookSearchApplication {
 
 	public static void main(String[] args) {
