96726ca842753c7ccc6075005ab2ad39bdfdeff1
hhlyn0328 <41069113+hhlyn0328@users.noreply.github.com>
2018-07-15 22:36:45 +0900

    1. 회원가입 validation 추가

--- book_search/src/main/java/com/kabank/book/controller/MemberController.java
+++ book_search/src/main/java/com/kabank/book/controller/MemberController.java
@@ -2,9 +2,15 @@ package com.kabank.book.controller;
 
 import java.util.Arrays;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,19 +37,49 @@ public class MemberController {
 		return view;
 	}
 	@GetMapping("/join")
-	public void join() { 
-
+	public ModelAndView join() { 
+		ModelAndView modelAndView = new ModelAndView();
+		Member member = new Member();
+		modelAndView.addObject("member", member);
+		modelAndView.setViewName("/join");
+		return modelAndView;
 	}
 	
 	//회원가입 저장	
 	@PostMapping("/join")
-	public String create(@ModelAttribute("member") Member member) {
-		Role role = new Role();
-		member.setUpw(pwEncoder.encode(member.getUpw()));
-		role.setRoleName("BASIC");
-		member.setRoles(Arrays.asList(role));
-		memberRepository.save(member);
-		return "redirect:/login";
+	public ModelAndView create(HttpServletRequest request, @ModelAttribute("member") @Valid Member member, BindingResult bindingResult) {
+		ModelAndView modelAndView = new ModelAndView();
+		if(member.getUid() == null || member.getUid() == "") {
+    		FieldError uidError = new FieldError("Member", "uid", "아이디는 필수항목입니다.");
+    		bindingResult.addError(uidError);
+    	} else if(memberRepository.findByUid(member.getUid())!=null) {
+    		FieldError uidDupError = new FieldError("Member", "uid", "중복된 아이디입니다.");
+    		bindingResult.addError(uidDupError);
+    	} 
+    	
+    	if(member.getUname() == null || member.getUname() == "") {
+    		FieldError unameError = new FieldError("Member", "uname", "이름은 필수항목입니다.");
+    		bindingResult.addError(unameError);
+    	}	
+    	
+    	if(member.getUpw() == null || member.getUpw() == ""){
+    		FieldError upwError = new FieldError("Member", "upw", "비밀번호는 필수항목입니다.");
+    		bindingResult.addError(upwError);
+    	} 
+		
+    	if(!bindingResult.hasErrors()) {
+    		Role role = new Role();
+    		member.setUpw(pwEncoder.encode(member.getUpw()));
+    		role.setRoleName("BASIC");
+    		member.setRoles(Arrays.asList(role));
+    		memberRepository.save(member);
+    		modelAndView.setViewName("redirect:/login");
+    		return modelAndView;
+    	}
+		
+    	modelAndView.setViewName("/join");
+		return modelAndView;
+		
 	}
 	
 	@RequestMapping("/login")
